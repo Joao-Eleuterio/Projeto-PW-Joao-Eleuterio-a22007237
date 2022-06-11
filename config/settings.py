df0d8de00830b602aefa35f7803031b4c29fd487
João Eleutério a22007237 <79324505+Joao-Eleuterio@users.noreply.github.com>
2022-06-11 22:32:06 +0100

    alterações

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/portfolio/static/'    # substitua nome_aplicacao pelo nome da sua aplicação
+STATIC_URL = '/portfolio/static/'  # substitua nome_aplicacao pelo nome da sua aplicação
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -141,5 +141,9 @@ CLOUDINARY_STORAGE = {
 }
 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static')), str(BASE_DIR.joinpath('media'))]  # novo se a pasta static estiver na pasta da aplicação app, altere para str(BASE_DIR.joinpath('app/static'))
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))   # novo
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
+
+DATABASES = {
+    "default": env.dj_db_url("DATABASE_URL")
+}
