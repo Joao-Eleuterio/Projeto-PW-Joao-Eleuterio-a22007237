49323648f4c1d57a4a75c6f2e43e3a89c5c11d06
João Eleutério a22007237 <79324505+Joao-Eleuterio@users.noreply.github.com>
2022-06-13 11:24:04 +0100

    alterações

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,6 @@ from environs import Env
 
 # Necessario para as imagens
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,13 +50,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # novo
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # novo
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -121,8 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
-STATIC_URL = '/portfolio/static/'  # substitua nome_aplicacao pelo nome da sua aplicação
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,18 +127,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Necessario para as imagens
 MEDIA_URL = '/portfolio/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': "joaoeleuterio",
     'API_KEY': "222161335646556",
     'API_SECRET': "Zc-b3uHN_dZTPIphHDsdzR3NZPc",
-}
-
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static')), str(BASE_DIR.joinpath('media'))]  # novo se a pasta static estiver na pasta da aplicação app, altere para str(BASE_DIR.joinpath('app/static'))
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
-
-DATABASES = {
-    "default": env.dj_db_url("DATABASE_URL")
 }
