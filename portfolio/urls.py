94ed832dd26c623bc643ff6a5f20570818a7a4ec
João Eleutério a22007237 <79324505+Joao-Eleuterio@users.noreply.github.com>
2022-06-17 18:52:59 +0100

    alterações

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -12,25 +12,17 @@ urlpatterns = [
     path('novaCadeira', views.nova_Cadeira_view, name='novaCadeira'),
     path('editaLicenciatura/<int:licenciatura_id>', views.edita_Licenciatura_view, name='editaLicenciatura'),
     path('editaCadeira/<int:cadeira_id>', views.edita_Cadeira_view, name='editaCadeira'),
-    path('apagaLicenciatura/<int:cadeira_id>', views.licenciatura_page_view, name='apagaLicenciatura'),
 
     # PROJETOS
     path('projetos', views.projetos_page_view, name='projetos'),
     path('editaProjeto/<int:projeto_id>', views.edita_Projeto_view, name='editaProjeto'),
-    path('apagaprojetos/<int:projeto_id>', views.projetos_page_view, name='apagaprojetos'),
     path('novoProjeto', views.novo_Projeto_view, name='novoProjeto'),
 
     # FORMACAO
     path('formacao', views.formacao_page_view, name='formacao'),
-    path('editaformacao/<int:projetos_id>', views.formacao_page_view, name='editaformacao'),
-    path('apagaformacao/<int:projetos_id>', views.formacao_page_view, name='apagaformacao'),
-    path('novaformacao', views.formacao_page_view, name='novaformacao'),
 
     # CERTIFICADOS
     path('certificados', views.certificados_page_view, name='certificados'),
-    path('editacertificados/<int:certificados_id>', views.certificados_page_view, name='editacertificados'),
-    path('apagacertificados/<int:certificados_id>', views.certificados_page_view, name='apagacertificados'),
-    path('novacertificados', views.certificados_page_view, name='novacertificados'),
 
     # BLOG
     path('blog', views.blog_page_view, name='blog'),
@@ -53,7 +45,3 @@ urlpatterns = [
     path('login', views.login_page_view, name='login'),
     path('logout', views.logout_page_view, name='logout'),
 ]
-
-#TODO
-# video
-
